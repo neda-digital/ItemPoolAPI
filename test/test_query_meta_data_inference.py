5fd1f71029524d32dd8eb6a5bf7fa385af6521b9
Lisa <128850065+Sneeuwpop23@users.noreply.github.com>
2026-01-15 20:50:48 +0100

    Feature/sql (#10)
    * Query Metadata Inference Handler Metrics
    
    * feat: add logger
    
    * fix: revert explicit insertion of None object
    
    * fix: actually parse the query and only persist found expressions
    
    * test: adapt test
    
    * test: add exemplatory unit-test
    
    * fix: exclude unit-test-coverage-report from tracking
    
    * fix: rm pyc-files from tracking
    
    * Ergänzung Testfälle
    
    * Testfall: Exception abfragen
    
    ---------
    
    Co-authored-by: Paul Christ <paul.l.christ@web.de>

--- test/test_query_meta_data_inference.py
+++ test/test_query_meta_data_inference.py
@@ -2,6 +2,7 @@ from ..src.services.MetaDataInference.SQLDataInferenceHandlers.QueryMetaDataInfe
     QueryMetricsHandler,
 )
 from ..src.models.TaskMaterials.QueryTaskMaterial import QueryTaskMaterial
+import pytest
 
 query_metrics_handler = QueryMetricsHandler()
 
@@ -9,8 +10,8 @@ query_metrics_handler = QueryMetricsHandler()
 def test_answer():
     material = QueryTaskMaterial(
         query="SELECT a, b, sum(a) FROM foo JOIN bar WHERE a > b;",
-        dialect="postgres",
-        metadata=None,
+        dialect = "postgres",
+        metadata = None,
     )
     metadata = query_metrics_handler.infer_metadata(query_material=material)
     assert metadata == {
@@ -24,3 +25,107 @@ def test_answer():
         "Table": 2,
         "Where": 1,
     }
+
+def test_subquery():
+    material = QueryTaskMaterial(
+        query = "SELECT name FROM (SELECT name, id FROM users) AS sub WHERE id > 10;",
+        dialect = "postgres",
+        metadata = None,
+    )
+    metadata = query_metrics_handler.infer_metadata(query_material=material)
+    assert metadata == {
+        "Column": 4,
+        "From": 2,
+        "GT": 1,
+        "Identifier": 6,
+        "Select": 2,
+        "Subquery": 1,
+        "Table": 1,
+        "Where": 1,
+        "Literal": 1,
+        "TableAlias": 1,
+    }
+
+def test_grouping():
+    material = QueryTaskMaterial(
+        query = "SELECT organisation, COUNT(*) FROM employees GROUP BY organisation HAVING COUNT(*) > 5;",
+        dialect = "postgres",
+        metadata = None,
+    )
+    metadata = query_metrics_handler.infer_metadata(query_material=material)
+    assert metadata == {
+        "Select": 1,
+        "From": 1,
+        "Table": 1,
+        "Column": 2,
+        "Identifier": 3, 
+        "Count": 2,
+        "Group": 1,
+        "Having": 1,
+        "GT": 1,
+        "Literal": 1,
+        "Star": 2,
+    }
+
+def test_joins():
+    material = QueryTaskMaterial(
+        query = "SELECT a.id,b-vaö FROM table_a AS a LEFT JOIN table_b AS b ON a.id = b.a_id;",
+        dialect = "postgres",
+        metadata = None,
+    )
+    metadata = query_metrics_handler.infer_metadata(query_material=material)
+    assert metadata == {
+        "Select": 1,
+        "From": 1,
+        "Table": 2,
+        "Column": 5,
+        "Identifier": 12, 
+        "Join": 1,
+        "TableAlias": 2,
+        "EQ": 1,       
+        "Sub": 1,
+    }
+
+def test_union():
+    material = QueryTaskMaterial(
+        query = "SELECT name FROM organisation UNION SELECT name FROM services,",
+        dialect = "postgres",
+        metadata = None,
+    )
+    metadata = query_metrics_handler.infer_metadata(query_material=material)
+    assert metadata == {
+        "Select": 2,
+        "From": 2,
+        "Table": 2,
+        "Column": 2,
+        "Identifier": 4, 
+        "Union": 1,
+    }    
+
+def test_capitalization():
+    material = QueryTaskMaterial(
+        query="select A, B from FOO where A > 0;",
+        dialect="postgres",
+        metadata=None,
+    )
+    metadata = query_metrics_handler.infer_metadata(query_material=material)
+    assert metadata == {
+        "Column": 3,
+        "From": 1,
+        "GT": 1,
+        "Identifier": 4,
+        "Select": 1,
+        "Table": 1,
+        "Where": 1,
+        "Literal": 1,
+    }
+
+def test_invalid_query():
+    material = QueryTaskMaterial(
+        query="SELECT FROM WHERE JOIN",
+        dialect="postgres",
+        metadata=None,
+    )
+    with pytest.raises(Exception) as exceptionInfo:
+        query_metrics_handler.infer_metadata(query_material=material)
+    assert "Expected table name" in str(exceptionInfo.value)
